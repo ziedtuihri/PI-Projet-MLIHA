b06ad038ac2f62f51961c735247416afa1b38bdd
AnisBenOthman <anis12315@live.fr>
2024-05-30 16:13:51 +0100

    add and update function of purchase operation

--- controllers/achat.js
+++ controllers/achat.js
@@ -1,26 +1,33 @@
+import mongoose from "mongoose";
 import Achat from "../models/achat.js";
 
 export async function addOnce(req, res) {
   try {
     const { idClient, idProduit, quantite, prix, dateAchat } = req.body;
+    const commande = [];
 
-    const commande = prix.map((_, index) => ({
-      idClient: idClient[index],
-      idProduit: idProduit[index],
-      quantite: quantite[index],
-      prix: prix[index],
-      total: prix[index] * quantite[index],
-      dateAchat: new Date(dateAchat[index]),
-    }));
+    for (let i = 0; i < idProduit.length; i++) {
+      const index = commande.findIndex((x) => x.idProduit == idProduit[i]);
+      if (index == -1) {
+        commande.push({
+          idClient: idClient[i],
+          idProduit: idProduit[i],
+          quantite: quantite[i],
+          prix: prix[i],
+          total: prix[i] * quantite[i],
+          dateAchat: new Date(dateAchat[i]),
+        });
+      } else {
+        commande[index].quantite += quantite[i];
+        commande[index].total = commande[index].quantite * commande[index].prix;
+      }
+    }
 
     const total = commande.reduce(
       (accumulator, currentValue) => accumulator + currentValue.total,
       0
     );
 
-    console.log(prix);
-    console.log(quantite);
-    console.log(total);
     const achat = await Achat.create({
       commande: commande,
       somme: total,
@@ -35,6 +42,67 @@ export async function getAll(req, res) {
   try {
     const achat = await Achat.find();
     res.status(200).json({ message: "purchase list", achat });
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+}
+
+export async function updateOne(req, res) {
+  try {
+    let newAchat;
+    const achat = await Achat.aggregate([
+      { $match: { _id: new mongoose.Types.ObjectId(req.params.idAchat) } },
+      { $unwind: "$commande" },
+      {
+        $match: {
+          "commande._id": new mongoose.Types.ObjectId(req.params.idProduit),
+        },
+      },
+    ]);
+    if (achat.length > 0) {
+      const result = achat[0];
+
+      if (req.body.quantite > result.commande.quantite) {
+        const newQuantite = req.body.quantite - result.commande.quantite;
+        const difference = newQuantite * result.commande.prix;
+        const newSomme = difference + result.somme;
+        newAchat = await Achat.findOneAndUpdate(
+          {
+            "commande._id": req.params.idProduit,
+          },
+          {
+            $set: {
+              "commande.$.quantite": req.body.quantite,
+              "commande.$.total": req.body.quantite * result.commande.prix,
+              somme: newSomme,
+            },
+          },
+          { new: true }
+        );
+        res.status(200).json({ mesg: "update successfully", newAchat });
+      } else if (req.body.quantite < result.commande.quantite) {
+        const newQuantite = result.commande.quantite - req.body.quantite;
+        const difference = newQuantite * result.commande.prix;
+        const newSomme = result.somme - difference;
+        newAchat = await Achat.findOneAndUpdate(
+          {
+            "commande._id": req.params.idProduit,
+          },
+          {
+            $set: {
+              "commande.$.quantite": req.body.quantite,
+              "commande.$.total": req.body.quantite * result.commande.prix,
+              somme: newSomme,
+            },
+          },
+          { new: true }
+        );
+        res.status(200).json({ mesg: "update successfully", newAchat });
+      } else {
+        newAchat = await Achat.findOne({ _id: req.params.idAchat });
+        res.status(200).json({ mesg: "la quantite est la même", newAchat });
+      }
+    }
   } catch (e) {
     res.status(500).json({ error: e.message });
   }
--- routes/achat.js
+++ routes/achat.js
@@ -1,7 +1,8 @@
 import express from "express";
-import { getAll, addOnce } from "../controllers/achat.js";
+import { getAll, addOnce, updateOne } from "../controllers/achat.js";
 const router = express.Router();
 
 router.route("/").get(getAll).post(addOnce);
+router.route("/:idAchat/:idProduit").put(updateOne);
 
 export default router;
