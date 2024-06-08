d3d0e60d4af470925d39b64e0ad910b3f739b9ea
Ibrahim1 <ibrahim.fartoun@gmail.com>
2024-06-08 22:45:08 +0200

    modif .dev .conf twilio

---
+++ config/twilio.js
@@ -0,0 +1,4 @@
+// config/twilio.js
+export const accountSid = 'AC8a81adb20c52041397a2de3b5d49be3e';
+export const authToken = '36638e9a52cf2a36da4bf86928894345';
+export const fromPhoneNumber = '+13852208299';
